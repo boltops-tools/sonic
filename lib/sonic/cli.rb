74b74c45acb8a240d812ca8a26a1d6de17a62dec
Tung Nguyen <tongueroo@gmail.com>
2018-05-16 17:15:17 -0700

    move common base commands to Command class

--- lib/sonic/cli.rb
+++ lib/sonic/cli.rb
@@ -44,22 +44,5 @@ module Sonic
     def list(filter)
       List.new(options.merge(filter: filter)).run
     end
-
-    desc "completion *PARAMS", "Prints words for auto-completion."
-    long_desc Help.text("completion")
-    def completion(*params)
-      Completer.new(CLI, *params).run
-    end
-
-    desc "completion_script", "Generates a script that can be eval to setup auto-completion."
-    long_desc Help.text("completion_script")
-    def completion_script
-      Completer::Script.generate
-    end
-
-    desc "version", "prints version"
-    def version
-      puts VERSION
-    end
   end
 end
--- lib/sonic/command.rb
+++ lib/sonic/command.rb
@@ -15,6 +15,23 @@ end
 
 module Sonic
   class Command < Thor
+    desc "completion *PARAMS", "Prints words for auto-completion."
+    long_desc Help.text("completion")
+    def completion(*params)
+      Completer.new(CLI, *params).run
+    end
+
+    desc "completion_script", "Generates a script that can be eval to setup auto-completion."
+    long_desc Help.text("completion_script")
+    def completion_script
+      Completer::Script.generate
+    end
+
+    desc "version", "prints version"
+    def version
+      puts VERSION
+    end
+
     class << self
       def dispatch(m, args, options, config)
         # Allow calling for help via:
